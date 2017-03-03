88ea39aef22c3b7e7a939558cb155ab0de1b6888
khiemhuynh95 <khiemhuynh95@gmail.com>
2017-03-03 16:26:17 +0700

    first commit

---
+++ lightagent.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    
+    return "ok",200
