39fde97236727cc6e274376d788bc5cbe570bde3
khiemhuynh95 <khiemhuynh95@gmail.com>
2017-03-04 12:53:48 +0700

    no message

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from flask import make_response
 # Flask app should start in global layout
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def verify():
 	
@@ -19,13 +18,39 @@ def verify():
 @app.route('/', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-    #log(req)
-    print("Request:")
-    print(json.dumps(req, indent=4))
+    #print("Request:")
+    #print(json.dumps(req, indent=4))
+    result = req.get("result")
+    parameters = result.get("parameters")
+
+    action = parameters.get("action-type")
+    print(action)
+    color = parameters.get("color")
+    print(color)
+
+    process(action,color)
 
     return "ok", 200
 
+def process(action,color):
 
+	if action == "change":
+		if color == "":
+			pass
+		elif color == "red":
+			print("red") #replace for light action here
+		elif color == "blue":
+			print("blue")
+		elif color == "green":
+			print("green")
+		elif color == "yellow":
+			print("yellow")
+		else:
+			print("purple")
+	elif action == "turn on":
+		print("turn on")
+	else:
+		print("turn off")
 
 if __name__ == '__main__':
     app.run(debug=True)
